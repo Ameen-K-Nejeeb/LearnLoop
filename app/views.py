50ab5f8459b1fdcc50720a9158f859d1ca6eed7e
DUDEamee <ameenameen8701@gmail.com>
2024-05-17 00:00:45 +0530

    added:  Data base data's were printed console ==clicked to see what you are typed

--- app/views.py
+++ app/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . models import Room
 # Create your views here.
 
 
-rooms=[
-    {'id':1, 'name':'lets start learn python'},
-    {'id':2, 'name':'start learn django'},
-    {'id':3, 'name':'How far can you go'},
-    {'id':4, 'name':'you are the 5 people you hang around'},
-    {'id':5, 'name':'Lets get started'},
-    {'id':6, 'name':'Have a good journey'},
-]
+#rooms=[
+#    {'id':1, 'name':'lets start learn python'},
+#    {'id':2, 'name':'start learn django'},
+#    {'id':3, 'name':'How far can you go'},
+#    {'id':4, 'name':'you are the 5 people you hang around'},
+#    {'id':5, 'name':'Lets get started'},
+#    {'id':6, 'name':'Have a good journey'},
+#]
 
 
 def home(request,):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'base/home.html',context )
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
+#    room = None
+#    for i in rooms:
+#        if i['id'] == int(pk):
+#            room = i
     context = {'room':room}
 
     return render(request,'base/room.html',context)
