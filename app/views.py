435281768ca15ffa7d928a94c7b5de014d31b252
DUDEamee <ameenameen8701@gmail.com>
2024-05-16 23:02:50 +0530

    The loop and ==touch to see

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+rooms=[
+    {'id':1, 'name':'lets start learn python'},
+    {'id':2, 'name':'start learn django'},
+    {'id':3, 'name':'How far can you go'},
+    {'id':4, 'name':'you are the 5 people you hang around'},
+    {'id':5, 'name':'Lets get started'},
+    {'id':6, 'name':'Have a good journey'},
+]
+
+
+def home(request,):
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context )
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+
+    return render(request,'base/room.html',context)
+
