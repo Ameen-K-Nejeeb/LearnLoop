7ca95b23a6b8f79c0645121b5630eced8199b1f1
DUDEamee <ameenameen8701@gmail.com>
2025-04-12 17:10:23 +0530

    update : Integrated Costom  User Model

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,7 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -13,3 +14,5 @@ urlpatterns = [
     path('api/', include('base.api.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
